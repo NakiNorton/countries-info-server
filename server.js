647a45715662fb47967eeea66ae53ab7a990fc46
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-02 19:28:53 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express'); // import express
+const app = express();
+app.use(express.json());
+
+app.set('port', process.env.PORT || 8080);
+app.locals.title = 'Countries in Oceania'
+app.locals.countries = [
+  { 
+    name: 'New Zealand',
+    capital: 'Wellington',
+    officalLanguages: [ 'English', 'Maori', 'New Zealand Sign Language']
+  },
+  {
+    name: 'Samoa',
+    capital: 'Apia',
+    officalLanguages: ['Samoan', 'English']
+  },
+  {
+    name: 'Tonga',
+    capital: 'Nuku\'alofa',
+    officalLanguages: ['Tongan', 'English']
+  },
+]
+
+app.get('/', (request, response) => {
+  response.send('Welcome! Enjoy the stats!')
+})
